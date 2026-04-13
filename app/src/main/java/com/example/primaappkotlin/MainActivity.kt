ddd454e84a589056610499adc046381cefb63993
Michele Baldi <michele.baldi@stu.istitutoturoldo.edu.it>
2026-04-13 09:19:28 +0200

    Commit finale

--- app/src/main/java/com/example/primaappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/primaappkotlin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.primaappkotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    var
+    lateinit var campoInput: EditText
+    lateinit var bottone: Button
+    lateinit var showNumero: TextView
+    lateinit var checkBlocca: CheckBox
+    lateinit var checkInverti: CheckBox
+
+    // ----------------- //
+
+    var incremento: Int = 0
+    var totale: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +33,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        campoInput = findViewById(R.id.campoInput)
+        bottone = findViewById(R.id.bottone)
+        showNumero = findViewById(R.id.showNumero)
+        checkBlocca = findViewById(R.id.checkBlocca)
+        checkInverti = findViewById(R.id.checkInverti)
+
+
+
+
+        bottone.setOnClickListener {
+            if (checkBlocca.isChecked == false){
+                incremento = campoInput.text.toString().toInt()
+                if (checkInverti.isChecked == true){
+                    totale = totale-incremento
+                    showNumero.text = totale.toString()
+                }
+                else{
+                    totale = totale+incremento
+                    showNumero.text = totale.toString()
+                }
+            }
+
+        }
+
+
     }
 }
